d045ca3329f28cf85c13350117e4b2aa3051970a
Polinet <uchpolinet@gmail.com>
2020-01-29 16:13:49 +1100

    created eventListener for each navLinks

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
+// Query selecting class name nav-toggle
 const navToggle = document.querySelector(".nav-toggle");
+
+//Query all classes name navLinks 
+const navLink = document.querySelectorAll(".navLinks")
 
 navToggle.addEventListener("click", () => {
     document.body.classList.toggle("nav-open")
+});
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        document.body.classList.remove("nav-open")
+    })
 })
